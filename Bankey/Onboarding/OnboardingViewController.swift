b8b7017a8782943b0c617a3378ecf53e4e64e99b
Hariom kumar <hariomkumar4659@gmail.com>
2024-10-28 00:04:23 +0530

    Initial Commit

---
+++ Bankey/Onboarding/OnboardingViewController.swift
@@ -0,0 +1,72 @@
+//
+//  OnboardingViewController.swift
+//  Bankey
+//
+//  Created by Hariom Kumar on 27/10/24.
+//
+
+import UIKit
+
+class OnboardingViewController: UIViewController {
+    
+    let stackView = UIStackView()
+    let heroImageView = UIImageView()
+    let label = UILabel()
+    
+    let heroImageName: String
+    let titleText: String
+    
+    init(heroImageName: String, titleText: String) {
+        self.heroImageName = heroImageName
+        self.titleText = titleText
+        
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented - not using storyboards")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        style()
+        layout()
+    }
+}
+
+extension OnboardingViewController {
+    func style() {
+        view.backgroundColor = .systemBackground // ! dont forget
+        
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        stackView.axis = .vertical
+        stackView.spacing = 20
+        
+        // Image
+        heroImageView.translatesAutoresizingMaskIntoConstraints = false
+        heroImageView.contentMode = .scaleAspectFit
+        heroImageView.image = UIImage(named: heroImageName)
+        
+        // Label
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.textAlignment = .center
+        label.font = UIFont.preferredFont(forTextStyle: .title3)
+        label.adjustsFontForContentSizeCategory = true
+        label.numberOfLines = 0
+        label.text = "Bankey is faster, easier to use, and has a brand new look and feel that will make you feel like you are back in 1989."
+    }
+    
+    func layout() {
+        stackView.addArrangedSubview(heroImageView)
+        stackView.addArrangedSubview(label)
+        
+        view.addSubview(stackView)
+        
+        NSLayoutConstraint.activate([
+            stackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            stackView.leadingAnchor.constraint(equalToSystemSpacingAfter: view.leadingAnchor, multiplier: 1),
+            view.trailingAnchor.constraint(equalToSystemSpacingAfter: stackView.trailingAnchor, multiplier: 1)
+        ])
+    }
+}
